f4bcb8895801adc971fafb02db54464c822abedb
ZhenyuHu <cszhenyuhu@gmail.com>
2018-10-11 20:13:49 +0800

    use oop

--- src/jvm/heap/ParallelScavengeHeapExample.java
+++ src/jvm/heap/ParallelScavengeHeapExample.java
@@ -14,14 +14,27 @@ public class ParallelScavengeHeapExample {
         int k = 5;
         while (k>=0) {
             //4MB
-            int[] intArray = new int[1024*1024];
+            // [] intArray = new int[1024*1024];
+            IntObj[] intArray = new IntObj[1024*1024];
             for (int i = 0; i < intArray.length; i++) {
-                intArray[i] = random.nextInt();
+                intArray[i] = new IntObj(random.nextInt());
             }
             for (int i = 0; i< 10; i++){
                 System.out.println(intArray[i]);
             }
             k--;
         }
+    }
+
+}
+class IntObj{
+    int value;
+    IntObj(int val){
+        value = val;
+    }
+
+    @Override
+    public String toString() {
+        return String.valueOf(value);
     }
 }
