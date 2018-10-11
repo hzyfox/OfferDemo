b11490facb3f08d7e92924b8e41cad8d0626006f
ZhenyuHu <cszhenyuhu@gmail.com>
2018-10-11 17:11:30 +0800

    New example

--- src/jvm/heap/ParallelScavengeHeapExample.java
+++ src/jvm/heap/ParallelScavengeHeapExample.java
@@ -9,13 +9,19 @@ import java.util.Random;
 
 public class ParallelScavengeHeapExample {
     public static void main(String[] args) {
+        System.out.println(System.getProperty("java.class.path"));
         Random random = new Random();
-        int[] intArray = new int[102400];
-        for (int i = 0; i < intArray.length; i++) {
-            intArray[i] = random.nextInt();
-        }
-        for (int i = 0; i< 10; i++){
-            System.out.println(intArray[i]);
+        int k = 5;
+        while (k>=0) {
+            //4MB
+            int[] intArray = new int[1024*1024];
+            for (int i = 0; i < intArray.length; i++) {
+                intArray[i] = random.nextInt();
+            }
+            for (int i = 0; i< 10; i++){
+                System.out.println(intArray[i]);
+            }
+            k--;
         }
     }
 }
