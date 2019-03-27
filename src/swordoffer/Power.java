a1e2a6ff9efbe97c9ededdae1654bb90c970b266
ZhenyuHu <cszhenyuhu@gmail.com>
2019-03-27 22:00:35 +0800

    add Power

--- src/swordoffer/Power.java
+++ src/swordoffer/Power.java
@@ -5,6 +5,29 @@ package swordoffer;
  * USER: husterfox
  */
 public class Power {
+    public double Power(double base, int exponent) {
+        if (exponent == 0) {
+            return 1;
+        }
+        if (exponent == Integer.MIN_VALUE) {
+            return 0;
+        }
+        boolean negative = false;
+        if (exponent < 0) {
+            negative = true;
+            exponent = -exponent;
+        }
+        double result;
+        if ((exponent & 1) == 0) {
+            result = Power(base, exponent / 2) * Power(base, exponent / 2) * base;
+        } else {
+            result = Power(base, exponent / 2) * Power(base, exponent / 2);
+        }
+        return negative ? 1 / result : result;
+    }
 
+    public static void main(String[] args) {
+
+    }
 
 }
