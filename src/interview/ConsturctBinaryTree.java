00df20ff2c3e36353f80c49ada2db087091a611b
孤月 <guyue.hzy@alibaba-inc.com>
2019-09-06 10:34:07 +0800

    add comments in interview

--- src/interview/ConsturctBinaryTree.java
+++ src/interview/ConsturctBinaryTree.java
@@ -1,30 +1,63 @@
 package interview;
 
+import leetcode.structure.ListNode;
+import leetcode.structure.TreeNode;
+import leetcode.util.Helper;
+
+import java.util.ArrayList;
+
 /**
  * 求1-n 个数 能构建二叉搜索树的数目
  */
 public class ConsturctBinaryTree {
-    public int numTrees(int n) {
+    public static int numTrees(int n) {
         int[] cache = new int[n];
         return cal(n, cache);
     }
-    public int cal(int n, int[]cache){
-        if(n == 0 || n==1){
+
+    public static int cal(int n, int[] cache) {
+        if (n == 0 || n == 1) {
             return 1;
         }
-        if(cache[n-1] != 0){
-            return cache[n-1];
+        if (cache[n - 1] != 0) {
+            return cache[n - 1];
         }
         int sum = 0;
-        for(int i = 0; i<= n; i++){
-            sum += cal(i, cache) * cal(n-i-1, cache);
+        for (int i = 1; i <= n; i++) {
+            sum += cal(i-1, cache) * cal(n - i, cache);
         }
-        cache[n-1] = sum;
+        cache[n - 1] = sum;
         return sum;
     }
 
+    public static ArrayList<TreeNode> dfsTree(int start, int end) {
+        ArrayList<TreeNode> ans = new ArrayList<>();
+        if (start == end) {
+            ans.add(new TreeNode(start));
+            return ans;
+        }
+        if (start > end) {
+            ans.add(null);
+            return ans;
+        }
+
+        for (int i = start; i <= end; i++) {
+            for (TreeNode left : dfsTree(start, i - 1)) {
+                for (TreeNode right : dfsTree(i + 1, end)) {
+                    TreeNode root = new TreeNode(i);
+                    root.left = left;
+                    root.right = right;
+                    ans.add(root);
+                }
+            }
+        }
+        return ans;
+    }
+
     public static void main(String[] args) {
-        ConsturctBinaryTree consturctBinaryTree = new ConsturctBinaryTree();
-        System.out.println(consturctBinaryTree.numTrees(3));
+        for(TreeNode root: dfsTree(1,3)){
+            System.out.println(Helper.treeNodeToString(root));
+        }
+        System.out.println(numTrees(3));
     }
 }
