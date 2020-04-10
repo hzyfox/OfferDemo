957aaaecd1bc92ec1dff5a7f1a51018f0240aae3
孤月 <cszhenyuhu@foxmail.com>
2020-04-10 19:17:52 +0800

    add pdd solution1

---
+++ src/Test/Solution1.java
@@ -0,0 +1,35 @@
+package Test;
+
+import java.util.PriorityQueue;
+import java.util.Scanner;
+
+/**
+ * create with Test
+ * USER: husterfox
+ */
+public class Solution1 {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int num = Integer.valueOf(scanner.nextLine());
+        String[] priceStr = scanner.nextLine().split(" ");
+        int[] prices = new int[num];
+        for (int i = 0; i < prices.length; i++) {
+            prices[i] = Integer.valueOf(priceStr[i]);
+        }
+
+        PriorityQueue<Integer> priorityQueue = new PriorityQueue<>((a, b) -> b - a);
+        for (int elem : prices) {
+            priorityQueue.offer(elem);
+        }
+        int ans = 0;
+        while (priorityQueue.size() >= 3) {
+            ans += priorityQueue.poll();
+            ans += priorityQueue.poll();
+            priorityQueue.poll();
+        }
+        while(!priorityQueue.isEmpty()){
+            ans += priorityQueue.poll();
+        }
+        System.out.println(ans);
+    }
+}
